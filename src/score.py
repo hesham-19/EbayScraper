21776ad2113ac42a0e0bcf0a2500264c762e1f97
Work <Work@Nagats-MacBook-Air.local>
2020-03-13 15:00:25 +0300

    extended range

--- src/score.py
+++ src/score.py
@@ -1,12 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+ebay_URL = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1312.R1.TR0.TRC0.A0.H0.TRS2&_nkw=phone&_sacat=0"
 # Scrapes an ebay webpage and list the names of the items in the page
 
 
-response = requests.get(
-    "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1312.R1.TR0.TRC0.A0.H0.TRS2&_nkw=phone&_sacat=0")
+response = requests.get(ebay_URL)
 print("Status is ", response.status_code)
 if response.status_code != 200:
     print("Error..")
@@ -19,5 +18,5 @@ else:
     soup = BeautifulSoup(html, "html.parser")
     items = soup.findAll("div", {"class": "s-item__wrapper clearfix"})
     print(len(items))
-    for item in range(3):
+    for item in range(len(items)):
         print(items[item].h3.text + "\n")
